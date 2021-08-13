b4947505a556434d13bb50e400aac70f8f2f27c3
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2021-08-13 18:12:40 +0200

    optimisé le carousel

--- DcodeCarousel/carousel.js
+++ DcodeCarousel/carousel.js
@@ -1,11 +1,11 @@
+/* identifier les items de chaque carousel */
 document.querySelectorAll(".carousel").forEach((carousel) => {
   const item = carousel.querySelectorAll(".items");
+  /* pour chaque item, créer un span class=button, avec le n0 de l'item comme textContent */
   const buttonsHtml = Array.from(item, (i) => {
-      console.log(i.textContent)
-      return `<span class="button">${
-        i.dataset.id[i.dataset.id.length - 1]
-      }</span> `;
+    return `<span class="button">${i.dataset.id}</span> `;
   });
+  /* inserer en derniere ligne du carousel, une dic "Nav" avec tous les spans */
   carousel.insertAdjacentHTML(
     "beforeend",
     `
@@ -13,40 +13,44 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
         ${buttonsHtml.join("")} 
         </div > `
   );
+  /* ecouter chaque bouton, enlever les choisis et rajouter au bouton cliqué */
   const buttons = carousel.querySelectorAll(".button");
-    let ind = 0;
   buttons.forEach((button, i) => {
     button.addEventListener("click", () => {
-      item.forEach((it) => it.classList.remove("item_choisi"));
-      buttons.forEach((but) => but.classList.remove("button_choisi"));
-      button.classList.add("button_choisi");
-        item[i].classList.add("item_choisi");
-        ind = i;
-        console.log(ind)
+      effacer();
+      rajouter(i);
     });
   });
-
-  let i = ind;
+  /* ecouter le clavier fleches gauche ou droite */
   document.addEventListener("keydown", (e) => {
-    item.forEach((it) => it.classList.remove("item_choisi"));
-    buttons.forEach((but) => but.classList.remove("button_choisi"));
+    let i = 0;
+    /* retrouver le n° du span button_choisi */
+    document.querySelectorAll("span").forEach((span, ind) => {
+      if (span.className === "button button_choisi") {
+        i = ind;
+      }
+    });
+    effacer();
     if (e.key === "ArrowRight") {
       i = i + 1;
-      if (i > buttons.length - 1) {
-        i = 0;
-      }
+      if (i > buttons.length - 1) i = 0;
     }
     if (e.key === "ArrowLeft") {
       i = i - 1;
-      if (i < 0) {
-        i = buttons.length - 1;
-      }
+      if (i < 0) i = buttons.length - 1;
     }
-    buttons[i].classList.add("button_choisi");
-      item[i].classList.add("item_choisi");
-      ind = i;
-      console.log(ind)
+    rajouter(i);
   });
-  buttons[0].classList.add("button_choisi");
-  item[0].classList.add("item_choisi");
+  rajouter(0);
+
+  /* fonction pour effacer les _choisi */
+  function effacer() {
+    item.forEach((it) => it.classList.remove("item_choisi"));
+    buttons.forEach((but) => but.classList.remove("button_choisi"));
+  }
+  /* fonction pour arjouter les _choisi */
+  function rajouter(i) {
+    buttons[i].classList.add("button_choisi");
+    item[i].classList.add("item_choisi");
+  }
 });
